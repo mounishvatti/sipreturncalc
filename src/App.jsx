d62a8a5df9d8a107e758850a8f1f548893b2854d
mounishvatti <mounishvatti2002@gmail.com>
2024-08-11 15:16:41 +0530

    added lumpsum and topup option

--- src/App.jsx
+++ src/App.jsx
@@ -1,182 +1,57 @@
-import { useCallback, useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
+import DisplaySIP from "./components/DisplaySIP";
+import DisplayLumpsum from "./components/DisplayLumpsum";
+import DisplayTopup from "./components/DisplayTopup";
+import Footer from "./components/Footer";
 import "./App.css";
-import ComponentRange from "./components/ComponentRange";
-import { Doughnut } from "react-chartjs-2";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-ChartJS.register(ArcElement, Tooltip, Legend);
+import { useState } from "react";
 
 function App() {
-  const [amount, setAmount] = useState(25000); // Monthly investment
-  const [returns, setReturns] = useState(8); // Annual return rate in percentage
-  const [duration, setDuration] = useState(10); // Investment duration in years
-  const [investedAmount, setInvestedAmount] = useState(0); // Total invested amount
-  const [estReturn, setEstReturn] = useState(0); // Estimated return
-  const [futureValue, setFutureValue] = useState(0); // Future value of SIP
-  const [chartData, setChartData] = useState({
-    labels: ["Invested Amount", "Estimated Returns"],
-    datasets: [
-      {
-        data: [investedAmount, estReturn],
-        backgroundColor: ["rgb(24,26,41)", "rgb(140, 149, 226)"],
-        borderColor: "transparent",
-        hoverBackgroundColor: ["rgb(24,26,41)", "rgb(140, 149, 226)"], // Optional: colors on hover
-        hoverOffset: 4,
-      },
-    ],
-  });
+  const [state, setState] = useState("sip");
+  const [active, setActive] = useState("sip");
 
-  const calculateFutureValue = useCallback(() => {
-    const monthlyRate = returns / 12 / 100; // Convert annual rate to monthly and percentage to decimal
-    const totalInstallments = duration * 12; // Total number of installments (months)
-
-    // Future value of SIP formula
-    const fv =
-      amount *
-      ((Math.pow(1 + monthlyRate, totalInstallments) - 1) / monthlyRate) *
-      (1 + monthlyRate);
-
-    setFutureValue(Math.round(fv));
-  }, [amount, returns, duration]);
-
-  const calculateInvestedAmount = useCallback(() => {
-    setInvestedAmount(Math.round(amount * duration * 12));
-  }, [amount, duration]);
-
-  const calculateEstimatedReturn = useCallback(() => {
-    setEstReturn(Math.round(futureValue - investedAmount));
-  }, [futureValue, investedAmount]);
-  
-  useEffect(() => {
-    calculateFutureValue();
-    calculateInvestedAmount();
-    calculateEstimatedReturn();
-  }, [
-    amount,
-    returns,
-    duration,
-    calculateFutureValue,
-    calculateInvestedAmount,
-    calculateEstimatedReturn,
-  ]);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setChartData({
-        labels: ["Invested Amount", "Estimated Returns"],
-        datasets: [
-          {
-            data: [investedAmount, estReturn],
-            backgroundColor: ["rgb(24,26,41)", "rgb(140, 149, 226)"],
-            borderColor: "transparent",
-            hoverBackgroundColor: ["rgb(24,26,41)", "rgb(140, 149, 226)"], // Optional: colors on hover
-            hoverOffset: 4,
-          },
-        ],
-      });
-    }, 500);
-    return () => clearTimeout(timer);
-  }, [investedAmount, estReturn, futureValue, duration, returns, amount, calculateFutureValue, calculateInvestedAmount, calculateEstimatedReturn]);
-
-  const formatNumber = (num) => {
-    return num.toLocaleString();
-  };
-
-  const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      legend: {
-        display: true,
-        position: "bottom",
-        labels: {
-          padding: 20,
-        },
-      },
-      tooltip: {
-        callbacks: {
-          label: function (tooltipItem) {
-            return `${
-              tooltipItem.label
-            }: ₹${tooltipItem.raw.toLocaleString()}`;
-          },
-        },
-      },
-    },
+  const handleClick = (e) => {
+    setState(e.target.id);
+    setActive(e.target.id);
   };
 
   return (
     <>
       <Navbar />
-      <div className="flex flex-col md:flex-row items-center justify-between md:pt-32 font-sans">
-        <div className="border-white/20 border p-4 md:p-8 rounded-lg w-full md:w-1/2">
-          <ComponentRange
-            labeltext="Monthly Investment"
-            units="INR"
-            param1={amount}
-            setParam1={setAmount}
-            minval={0}
-            maxval={1000000}
-            stepcount={500}
-          />
-          <ComponentRange
-            labeltext="Expected return rate (p.a)"
-            units="%"
-            param1={returns}
-            setParam1={setReturns}
-            minval={1}
-            maxval={30}
-            stepcount={0.1}
-          />
-          <ComponentRange
-            labeltext="Time Period"
-            units="Yr"
-            param1={duration}
-            setParam1={setDuration}
-            minval={1}
-            maxval={40}
-            stepcount={1}
-          />
-        </div>
-        <div style={{width: "350px", height: "350px"}}>
-          <Doughnut data={chartData} options={options} />
-        </div>
+      <div className="flex flex-row justify-start gap-4 pb-6 pt-14 pl-8">
+        <button
+          id="sip"
+          className={`text-white rounded-full p-2 px-4 text-md ${
+            active === "sip" ? "bg-green-500/30" : ""
+          }`}
+          onClick={handleClick}
+        >
+          SIP
+        </button>
+        <button
+          id="lumpsum"
+          className={`text-white rounded-full p-2 px-4 text-md ${
+            active === "lumpsum" ? "bg-green-500/30" : ""
+          }`}
+          onClick={handleClick}
+        >
+          Lumpsum
+        </button>
+        <button
+          id="topup"
+          className={`text-white rounded-full p-2 px-4 text-md ${
+            active === "topup" ? "bg-green-500/30" : ""
+          }`}
+          onClick={handleClick}
+        >
+          Top Up
+        </button>
       </div>
+      {state === "sip" && <DisplaySIP />}
+      {state === "lumpsum" && <DisplayLumpsum />}
+      {state === "topup" && <DisplayTopup />}
 
-      <div className="flex flex-row md:flex-row justify-between p-20 md:p-9 w-full md:w-1/2 mx-auto">
-        <div className="mb-4 md:mb-0">
-          <h1 className="text-white/50 p-3">
-            Invested Amount{" "}
-            <span className="text-white/80 font-semibold">
-              ₹{formatNumber(investedAmount)}
-            </span>
-          </h1>
-        </div>
-        <div className="mb-4 md:mb-0">
-          <h1 className="text-white/50 p-3">
-            Estimated Return{" "}
-            <span className="text-white/80 font-semibold">
-              ₹{formatNumber(estReturn)}
-            </span>
-          </h1>
-        </div>
-        <div>
-          <h1 className="text-white/50 p-3">
-            Total Returns{" "}
-            <span className="text-white/80 font-semibold">
-              ₹{formatNumber(futureValue)}
-            </span>
-          </h1>
-        </div>
-      </div>
-      <div className="absolute inset-x-0 bottom-0 flex flex-col items-center justify-between p-4 md:p-9">
-        <p className="text-white/50 text-lg">
-          Made with ❤️ by{" "}
-          <a href="https://github.com/mounishvatti" className="text-green-300/50">
-            Mounish Vatti
-          </a>
-        </p>
-      </div>
+      <Footer />
     </>
   );
 }
